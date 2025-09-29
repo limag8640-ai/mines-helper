44b4f43f68f17f70b67d1b9d5804563746c70278
limag8640-ai <limag8640@gmail.com>
2025-09-28 23:28:57 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+const root = document.getElementById('root');
+root.innerHTML = `
+  <h1 style="font-family: sans-serif; text-align: center; margin-top: 50px;">
+    ✅ Arquivo app.js carregado com sucesso!
+  </h1>
+`;
